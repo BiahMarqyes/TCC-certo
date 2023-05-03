aae109984ebad4fffa61dfe65e9b48c6dd50bbde
Anazz24 <analauramoraes87@gmail.com>
2023-05-03 08:50:42 -0300

    inicio

---
+++ src/app/anotacoes/anotacoes.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anotacoes',
+  templateUrl: './anotacoes.page.html',
+  styleUrls: ['./anotacoes.page.scss'],
+})
+export class AnotacoesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'anotacoes',
+    loadChildren: () => import('./anotacoes/anotacoes.module').then( m => m.AnotacoesPageModule)
   },
+
 ];
 
 @NgModule({
